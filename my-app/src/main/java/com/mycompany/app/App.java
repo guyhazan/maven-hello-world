bc79ca151823f2c4feb05617f4ebc2df7f0aff25
GuyHazan <92712989+guyhazan@users.noreply.github.com>
2021-10-18 16:04:31 +0300

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
- /* Add your name to the "Hello World" message */
             
         System.out.println( "Hello World! - GuyHazan - " );
 
